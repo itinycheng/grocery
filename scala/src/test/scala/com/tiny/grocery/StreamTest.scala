eec9614ced60e65a993207461e849354e4e00967
tiny <tiny_wcl@163.com>
2017-03-03 11:18:20 +0800

    Stream test

--- scala/src/test/scala/com/tiny/grocery/StreamTest.scala
+++ scala/src/test/scala/com/tiny/grocery/StreamTest.scala
@@ -4,6 +4,8 @@ import java.util.concurrent.LinkedBlockingQueue
 
 import org.junit.Test
 
+import scala.collection.immutable.Stream
+
 /**
   * Created by 16072453 on 2016/10/31.
   */
@@ -12,13 +14,14 @@ class StreamTest {
   @Test
   def testContinually() = {
 
-    val queue = new LinkedBlockingQueue[String](10000);
-    for (i <- 1 to 10000) {
+    val queue = new LinkedBlockingQueue[String](10000000);
+    for (i <- 1 to 10000000) {
       queue.put(i + "fs" + i)
     }
     Stream.continually(queue.poll()).takeWhile(item => item != null && !item.isEmpty).foreach {
       curItem =>
-        println(curItem)
+        Thread.sleep(2);
+        queue.offer(curItem + "d")
         if(curItem.contains("10000000"))
           println("end")
     }
